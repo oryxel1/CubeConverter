b1476be053980209f98c7c61abfa1d8de55b36ee
Oryxel <172038521+Oryxel@users.noreply.github.com>
2024-09-12 21:58:27 +0700

    Happy accident ;).

--- src/main/java/org/oryxel/cube/converter/FormatConverter.java
+++ src/main/java/org/oryxel/cube/converter/FormatConverter.java
@@ -93,7 +93,7 @@ public class FormatConverter {
         double scale = getScalingSize(minFrom, maxTo);
         for (ItemModelData model : models) {
             scaleEverything(model.elements(), scale);
-            model.scale(scale);
+            model.scale(1 / scale);
         }
 
         return Collections.unmodifiableList(models);
